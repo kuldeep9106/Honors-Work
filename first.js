80b03d54573057d0ab3b5ad3663d686678e8618b
kuldeep <sahukuldeep044@gmail.com>
2022-07-14 08:55:16 +0530

    kuldeep make changes

--- first.js
+++ first.js
@@ -1,10 +1,17 @@
-const details = [
+const details = [                                           
     {
     name:"kuldeep",
     mobile:"8767876577",
     },
     {
     name:"vikas",
+    mobile:"9000876577",
+    },
+
+
+    
+    {
+    name:"chandni", 
     mobile:"9000876577",
     },
 ]
